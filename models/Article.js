98521d7708f4b497cbef761c7d6e1999cf0c74e4
unknown <dcryptOG.geoffdotson@gmail.com>
2018-10-10 06:09:19 -0500

    Added scrape

--- models/Article.js
+++ models/Article.js
@@ -1,4 +1,4 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
@@ -21,12 +21,12 @@ var ArticleSchema = new Schema({
   // This allows us to populate the Article with an associated Note
   note: {
     type: Schema.Types.ObjectId,
-    ref: "Note"
+    ref: 'Note'
   }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+var Article = mongoose.model('Article', ArticleSchema);
 
 // Export the Article model
 module.exports = Article;
